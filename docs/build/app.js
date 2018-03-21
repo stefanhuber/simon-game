5a8499edb780c0fc4bc56c8a5f5be96cae5c885f
Stefan Huber <stefan.huber@beyondit.at>
2018-03-21 15:22:56 +0100

    test

--- docs/build/app.js
+++ docs/build/app.js
@@ -1,2 +1,98 @@
 /*! Built with http://stenciljs.com */
-!function(t,e,n,r,i,o,a,c,s,u,l,p){for((t.App=t.App||{}).components=s,t.customElements||(t.$whenDefined=[],t.customElements={whenDefined:function(e){return{then:function(n){t.$whenDefined.push([e,n])}}}}),(l=s.filter(function(t){return t[2]}).map(function(t){return t[0]})).length&&((u=e.createElement("style")).innerHTML=l.join()+"{visibility:hidden}.hydrated{visibility:inherit}",u.setAttribute("data-styles",""),e.head.insertBefore(u,e.head.firstChild)),u=(l=e.querySelectorAll("script")).length-1;u>=0&&!(p=l[u]).src&&!p.hasAttribute("data-resources-url");u--);(l=p.getAttribute("data-resources-url"))&&(i=l),!i&&p.src&&(i=(l=p.src.split("/").slice(0,-1)).join("/")+(l.length?"/":"")+"app/"),u=e.createElement("script"),function(t,e,n,r){return!(e.search.indexOf("core=esm")>0)&&(!(!(e.search.indexOf("core=es5")>0||"file:"===e.protocol)&&t.customElements&&t.fetch&&t.CSS&&t.CSS.supports&&t.CSS.supports("color","var(--c)")&&"noModule"in n)||function(t){try{return new Function('import("")'),!1}catch(t){}return!0}())}(t,t.location,u)?u.src=i+"app.yxrpbav4.js":(u.src=i+"app.m9hna5wq.js",u.setAttribute("type","module"),u.setAttribute("crossorigin",!0)),u.setAttribute("data-resources-url",i),u.setAttribute("data-namespace","app"),e.head.appendChild(u)}(window,document,0,0,0,0,0,0,[["sn-app","uvwf6qvo",1],["sn-game","yigntril",1,[["animationPattern",5],["history",1]],0,[["gamepadTouched","gamepadTouched"]]],["sn-gameboard","yigntril",1,[["animationPattern",1],["step",5]],0,[["animationiteration","blinkFinished"]]],["sn-gameover","1upjtoxg",0,[["match",1]]],["sn-menu","epmpr0ju"],["stencil-route","uvwf6qvo",0,[["activeRouter",3,0,0,0,"activeRouter"],["component",1,0,1,2],["componentProps",1],["exact",1,0,1,3],["group",1,0,1,2],["groupIndex",1,0,1,4],["location",3,0,0,0,"location"],["match",5],["routeRender",1],["url",1]]],["stencil-route-link","d24cxzbj",0,[["activeClass",1,0,1,2],["activeRouter",3,0,0,0,"activeRouter"],["custom",1,0,1,2],["exact",1,0,1,3],["location",3,0,0,0,"location"],["match",5],["url",1,0,1,2],["urlMatch",1]]],["stencil-router","uvwf6qvo",0,[["activeRouter",3,0,0,0,"activeRouter"],["historyType",1],["match",5],["root",1,0,1,2],["titleSuffix",1,0,1,2]]]],void 0);
+(function(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCoreSsr, appCorePolyfilled, hydratedCssClass, components) {
+
+function init(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCorePolyfilled, hydratedCssClass, components, x, y, scriptElm) {
+    // create global namespace if it doesn't already exist
+    (win[namespace] = win[namespace] || {}).components = components;
+    if (!win.customElements) {
+        // temporary customElements polyfill only for "whenDefined"
+        // this is incase customElements.whenDefined('my-tag') is
+        // used before the polyfill is downloaded
+        win.$whenDefined = [];
+        win.customElements = {
+            whenDefined: function (tag) {
+                return {
+                    then: function (cb) {
+                        win.$whenDefined.push([tag, cb]);
+                    }
+                };
+            }
+        };
+    }
+    y = components.filter(function (c) { return c[2]; }).map(function (c) { return c[0]; });
+    if (y.length) {
+        // auto hide components until they been fully hydrated
+        // reusing the "x" and "i" variables from the args for funzies
+        x = doc.createElement('style');
+        x.innerHTML = y.join() + '{visibility:hidden}.' + hydratedCssClass + '{visibility:inherit}';
+        x.setAttribute('data-styles', '');
+        doc.head.insertBefore(x, doc.head.firstChild);
+    }
+    // figure out the script element for this current script
+    y = doc.querySelectorAll('script');
+    for (x = y.length - 1; x >= 0; x--) {
+        scriptElm = y[x];
+        if (scriptElm.src || scriptElm.hasAttribute('data-resources-url')) {
+            break;
+        }
+    }
+    // get the resource path attribute on this script element
+    y = scriptElm.getAttribute('data-resources-url');
+    if (y) {
+        // the script element has a data-resources-url attribute, always use that
+        resourcesUrl = y;
+    }
+    if (!resourcesUrl && scriptElm.src) {
+        // we don't have an exact resourcesUrl, so let's
+        // figure it out relative to this script's src and app's filesystem namespace
+        y = scriptElm.src.split('/').slice(0, -1);
+        resourcesUrl = (y.join('/')) + (y.length ? '/' : '') + fsNamespace + '/';
+    }
+    // request the core this browser needs
+    // test for native support of custom elements and fetch
+    // if either of those are not supported, then use the core w/ polyfills
+    // also check if the page was build with ssr or not
+    x = doc.createElement('script');
+    if (usePolyfills(win, win.location, x, 'import("")')) {
+        x.src = resourcesUrl + appCorePolyfilled;
+    }
+    else {
+        x.src = resourcesUrl + appCore;
+        x.setAttribute('type', 'module');
+        x.setAttribute('crossorigin', true);
+    }
+    x.setAttribute('data-resources-url', resourcesUrl);
+    x.setAttribute('data-namespace', fsNamespace);
+    doc.head.appendChild(x);
+}
+function usePolyfills(win, location, scriptElm, dynamicImportTest) {
+    // fyi, dev mode has verbose if/return statements
+    // but it minifies to a nice 'lil one-liner ;)
+    if (location.search.indexOf('core=esm') > 0) {
+        // force es2015 build
+        return false;
+    }
+    if ((location.search.indexOf('core=es5') > 0) ||
+        (location.protocol === 'file:') ||
+        (!win.customElements) ||
+        (!win.fetch) ||
+        (!(win.CSS && win.CSS.supports && win.CSS.supports('color', 'var(--c)'))) ||
+        (!('noModule' in scriptElm))) {
+        // force es5 build w/ polyfills
+        return true;
+    }
+    return doesNotSupportsDynamicImports(dynamicImportTest);
+}
+function doesNotSupportsDynamicImports(dynamicImportTest) {
+    try {
+        new Function(dynamicImportTest);
+        return false;
+    }
+    catch (e) { }
+    return true;
+}
+
+
+init(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCoreSsr, appCorePolyfilled, hydratedCssClass, components);
+
+})(window, document, "App","app",0,"app.core.js","es5-build-disabled.js","hydrated",[["sn-app","sn-app",1],["sn-game","sn-game",1,[["animationPattern",5],["history",1]],0,[["gamepadTouched","gamepadTouched"]]],["sn-gameboard","sn-game",1,[["animationPattern",1],["step",5]],0,[["animationiteration","blinkFinished"]]],["sn-gameover","sn-gameover",0,[["match",1]]],["sn-menu","sn-menu"],["stencil-route","sn-app",0,[["activeRouter",3,0,0,0,"activeRouter"],["component",1,0,1,2],["componentProps",1],["exact",1,0,1,3],["group",1,0,1,2],["groupIndex",1,0,1,4],["location",3,0,0,0,"location"],["match",5],["routeRender",1],["url",1]]],["stencil-route-link","stencil-route-link",0,[["activeClass",1,0,1,2],["activeRouter",3,0,0,0,"activeRouter"],["custom",1,0,1,2],["exact",1,0,1,3],["location",3,0,0,0,"location"],["match",5],["url",1,0,1,2],["urlMatch",1]]],["stencil-router","sn-app",0,[["activeRouter",3,0,0,0,"activeRouter"],["historyType",1],["match",5],["root",1,0,1,2],["titleSuffix",1,0,1,2]]]]);
